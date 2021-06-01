187651e3850cff39ce5f2c76a3fcce9a080c22e9
sheridanwinter2021 <sivagama.srinivasan@sheridanc.on.ca>
2021-06-01 13:17:35 -0400

    save first student

---
+++ src/studentdetails/Student.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentdetails;
+
+/**
+ * data for student - model class
+ * mvc - controller - next example - running the logic
+ * @author sivagamasrinivasan
+ */
+public class Student 
+{
+private String name;
+private int age;
+private int marks;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * @return the marks
+     */
+    public int getMarks() {
+        return marks;
+    }
+
+    /**
+     * @param marks the marks to set
+     */
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+
+    
+
+
+
+}
---
+++ src/studentdetails/StudentDetails.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentdetails;
+
+/**
+ * MVC  - organizing code
+ * view class
+ * @author sivagamasrinivasan
+ */
+public class StudentDetails {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+       Student[] list = new Student[2]; //array of object 
+       
+       Student s1 = new Student();
+        s1.setName("peter"); //incorrect mark - avoid direct and incorrect marks - encapsulation principle
+       s1.setAge(23);       
+               
+       Student s2 = new Student();      
+       s2.setName("john"); //incorrect mark - avoid direct and incorrect marks - encapsulation principle
+       s2.setAge(23);
+       
+       list[0]= s1;
+       list[1]= s2; //stored object in array
+      
+       System.out.println(list[0].getName() + list[0].getAge() + " "+ list[1].getName() + list[1].getAge());
+
+
+    }
+    
+}
